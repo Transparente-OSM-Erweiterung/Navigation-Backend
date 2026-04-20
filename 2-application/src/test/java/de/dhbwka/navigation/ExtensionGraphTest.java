6257c90328b2785248717b26562948a4af3e8abf
vltir <87820976+vltir@users.noreply.github.com>
2026-04-20 19:21:55 +0200

    Test refactor

--- 2-application/src/test/java/de/dhbwka/navigation/ExtensionGraphTest.java
+++ 2-application/src/test/java/de/dhbwka/navigation/ExtensionGraphTest.java
@@ -4,144 +4,87 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import java.util.Collection;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ExtensionGraphTest {
 
-    @Test
-    void getEdgesOfBaseNodeShouldAssignCorrectIds() {
-        GeoTestNode main = new GeoTestNode("0", 0, 0);
-        List<GeoTestNode> neighboursOfMain = List.of(
-                new GeoTestNode("1", 0, 1),
-                new GeoTestNode("2", 1, 0),
-                new GeoTestNode("3", 0, -1),
-                new GeoTestNode("4", -1, 0)
-        );
-
+    private ExtensionGraph<GeoTestNode, GeoTestEdge> createSetup(GeoTestNode main, List<GeoTestNode> neighbors) {
         InMemoryGraph<GeoTestNode, GeoTestEdge> graph = new InMemoryGraph<>();
         graph.addNode(main);
-        neighboursOfMain.forEach(node -> {
+        neighbors.forEach(node -> {
             graph.addNode(node);
             graph.addEdge(new GeoTestEdge(main, node));
             graph.addEdge(new GeoTestEdge(node, main));
         });
+        return new ExtensionGraph<>(graph);
+    }
 
-        ExtensionGraph<GeoTestNode, GeoTestEdge> extGraph = new ExtensionGraph<>(graph);
+    @Test
+    void getEdgesOfBaseNodeShouldAssignCorrectIds() {
+        GeoTestNode main = new GeoTestNode("0", 0, 0);
+        var neighbors = List.of(
+                new GeoTestNode("1", 0, 1), new GeoTestNode("2", 1, 0),
+                new GeoTestNode("3", 0, -1), new GeoTestNode("4", -1, 0)
+        );
 
-        Collection<CategorizedGeoEdge<? extends GeoNode, ExtensionEdgeCategory>> extEdges = extGraph.getEdgesFrom(main.getId());
+        var extGraph = createSetup(main, neighbors);
 
-        assertThat(extEdges)
-                .hasSize(8)
+        assertThat(extGraph.getEdgesFrom(main.getId()))
                 .extracting(e -> e.getDestination().getId())
-                .as("generated Node Ids should be of the expected pattern")
                 .containsExactlyInAnyOrder("1", "2", "3", "4", "0a", "0b", "0c", "0d");
     }
 
     @Test
     void getEdgesOfExtensionNodeShouldAssignCorrectIds() {
-        GeoTestNode origin = new GeoTestNode("0a", 1, 1);
         GeoTestNode main = new GeoTestNode("0", 0, 0);
-        List<GeoTestNode> neighboursOfMain = List.of(
-                new GeoTestNode("1", 0, 1),
-                new GeoTestNode("2", 1, 0),
-                new GeoTestNode("3", 0, -1),
-                new GeoTestNode("4", -1, 0)
+        var neighbors = List.of(
+                new GeoTestNode("1", 0, 1), new GeoTestNode("2", 1, 0),
+                new GeoTestNode("3", 0, -1), new GeoTestNode("4", -1, 0)
         );
 
-        InMemoryGraph<GeoTestNode, GeoTestEdge> graph = new InMemoryGraph<>();
-        graph.addNode(main);
-        neighboursOfMain.forEach(node -> {
-            graph.addNode(node);
-            graph.addEdge(new GeoTestEdge(main, node));
-            graph.addEdge(new GeoTestEdge(node, main));
-        });
+        var extGraph = createSetup(main, neighbors);
 
-        ExtensionGraph<GeoTestNode, GeoTestEdge> extGraph = new ExtensionGraph<>(graph);
-
-        Collection<CategorizedGeoEdge<? extends GeoNode, ExtensionEdgeCategory>> extEdges = extGraph.getEdgesFrom(origin.getId());
-
-        assertThat(extEdges)
-                .hasSize(5)
+        // Wir fragen die Kanten eines virtuellen Knotens ab
+        assertThat(extGraph.getEdgesFrom("0a"))
                 .extracting(e -> e.getDestination().getId())
-                .as("generated Node Ids should be of the expected pattern")
                 .containsExactlyInAnyOrder("0", "0b", "0d", "1a", "2a");
     }
 
     @Test
     void getNeighborsPositioningRealOsmNode() {
         GeoTestNode main = new GeoTestNode("1599059100", 49.0040323, 8.4000678);
-        List<GeoTestNode> neighboursOfMain = List.of(
+        var neighbors = List.of(
                 new GeoTestNode("1794126907", 49.0040313, 8.4000282),
                 new GeoTestNode("21533398", 49.0040308, 8.4002886),
                 new GeoTestNode("1600203549", 49.0040778, 8.4000694),
                 new GeoTestNode("12599995589", 49.0039223, 8.4000615)
         );
 
-        InMemoryGraph<GeoTestNode, GeoTestEdge> graph = new InMemoryGraph<>();
-        graph.addNode(main);
-        neighboursOfMain.forEach(node -> {
-            graph.addNode(node);
-            graph.addEdge(new GeoTestEdge(main, node));
-            graph.addEdge(new GeoTestEdge(node, main));
-        });
+        var extGraph = createSetup(main, neighbors);
 
-        ExtensionGraph<GeoTestNode, GeoTestEdge> extGraph = new ExtensionGraph<>(graph);
-
-        Collection<CategorizedGeoEdge<? extends GeoNode, ExtensionEdgeCategory>> extEdges = extGraph.getEdgesFrom(main.getId());
-
-        assertThat(extEdges)
-                .hasSize(8)
+        assertThat(extGraph.getEdgesFrom(main.getId()))
                 .extracting(e -> e.getDestination().getId())
-                .as("generated Node Ids should be of the expected pattern")
-                .containsExactlyInAnyOrder("1794126907", "21533398", "1600203549", "12599995589", "1599059100a", "1599059100b", "1599059100c", "1599059100d");
+                .containsExactlyInAnyOrder("1794126907", "21533398", "1600203549", "12599995589",
+                                          "1599059100a", "1599059100b", "1599059100c", "1599059100d");
     }
 
-    @ParameterizedTest(name = "Angle of Node {0} ({1}|{2}) should be {3} degrees")
+    @ParameterizedTest(name = "Angle of Node {0} ({1}|{2}) should be {3}°")
     @CsvSource({
-        "1,  0,  1,   0",
-        "2,  1,  1,  45",
-        "3,  1,  0,  90",
-        "4,  1, -1, 135",
-        "5,  0, -1, 180",
-        "6, -1, -1, 225",
-        "7, -1,  0, 270",
-        "8, -1,  1, 315"
+        // Normierte Distanzen
+        "1,  0,  1,   0", "2,  1,  1,  45", "3,  1,  0,  90", "4,  1, -1, 135",
+        "5,  0, -1, 180", "6, -1, -1, 225", "7, -1,  0, 270", "8, -1,  1, 315",
+        // Nicht-normierte Distanzen (skaliert)
+        "1s, 0,  2,   0", "2s, 2,  2,  45", "3s, 2,  0,  90", "4s, 2, -2, 135",
+        "5s, 0, -2, 180", "6s, -2, -2, 225", "7s, -2,  0, 270", "8s, -2,  2, 315"
     })
-    void calcDegShouldReturnCorrectDegrees(String id, double x, double y, double expectedDeg) {
+    void calcDeg_ShouldReturnCorrectDegrees(String id, double x, double y, double expectedDeg) {
         GeoNode origin = new GeoTestNode("0", 0, 0);
         GeoNode target = new GeoTestNode(id, x, y);
 
-        double actualDeg = ExtensionGraph.calcDeg(origin, target);
-
-        assertThat(actualDeg)
-            .as("Angle of Destination Node %s", id)
-            .isEqualTo(expectedDeg);
-    }
-
-    @ParameterizedTest(name = "Angle of Node {0} ({1}|{2}) should be {3} degrees")
-    @CsvSource({
-        "1,  0,  2,   0",
-        "2,  2,  2,  45",
-        "3,  2,  0,  90",
-        "4,  2, -2, 135",
-        "5,  0, -2, 180",
-        "6, -2, -2, 225",
-        "7, -2,  0, 270",
-        "8, -2,  2, 315"
-    })
-    void calcDegShouldHandleNonNormalizedDistances(String id, double x, double y, double expectedDeg) {
-        GeoNode origin = new GeoTestNode("0", 0, 0);
-        GeoNode target = new GeoTestNode(id, x, y);
-
-        double actualDeg = ExtensionGraph.calcDeg(origin, target);
-
-        assertThat(actualDeg)
-            .as("Angle of Node %s at (%s, %s)", id, x, y)
+        assertThat(ExtensionGraph.calcDeg(origin, target))
+            .as("Calculated angle for node %s", id)
             .isEqualTo(expectedDeg);
     }
 }
